597ac4d22caacc29c0bae0c2950f684be3e214e5
Jerome Carless <Jerome@nbp-205-30.nbp.ruw.rutgers.edu>
2016-04-13 18:12:51 -0400

    couldnt push objects to txt correctly

--- bus.js
+++ bus.js
@@ -5,24 +5,21 @@ var bus = function(driverName, color, gas){
 	this.gas = parseInt(gas);
 	this.studentEntersBus = function(student){
 		this.studentsOnTheBus.push(student);
-		console.log('student enters');
-		console.log(this.studentsOnTheBus);
 	};
 	this.busChatter = function(){
 		for (var i = 0; i < this.studentsOnTheBus.length; i++) {
 			if (this.studentsOnTheBus[i].fun == true) {
 				console.log(this.studentsOnTheBus[i].catchPhrase);
+				
 			}
 		};
 	};
 	this.removeStudent = function(student){
 		for (var i = 0; i < this.studentsOnTheBus.length; i++) {
-			if (student == this.studentsOnTheBus[i].name) {
+			if (student === this.studentsOnTheBus[i].name) {
 			console.log(this.studentsOnTheBus[i].name + ' was removed');
 			this.studentsOnTheBus.splice(i, 1);
 			
-			}else{
-				console.log('no students removed');
 			};
 		};
 	}
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+var fs = require('fs');
 var prompt = require('prompt');
 var student = require('./student.js');
 var bus = require('./bus.js');
 var newStudent = '';
+var aStudent = '';
 var addStudent = function(){
 	prompt.start();
   prompt.get(['name', 'gender', 'grade', 'gpa', 'detentions', 'sleepingInClass', 'catchPhrase', 'addAnotherStudent', 'Remove_This_Student'], function (err, result) {
@@ -10,12 +12,20 @@ var addStudent = function(){
   	newStudent.canStudentHaveFun();
   	newBus.removeStudent(result.Remove_This_Student);
   	newBus.studentEntersBus(newStudent);
+    // aStudent = JSON.stringify(newBus.studentsOnTheBus);
+    // fs.writeFile('students.txt', aStudent, function(err){
 
-  	if (newBus.studentsOnTheBus.length >= 3) {
+    // });
+    // console.log(newStudent);
+    // console.log(newBus.studentsOnTheBus);
+    // console.log(aStudent);
+    // console.log(newBus.studentsOnTheBus.length);
+  	if (newBus.studentsOnTheBus.length >= 5) {
    		newBus.busChatter();
    	}
 
   	if (result.addAnotherStudent === 'yes') {
+      console.log(newBus.studentsOnTheBus);
   		addStudent();
   	}
 
@@ -23,6 +33,13 @@ var addStudent = function(){
 };
 
 var newBus = new bus('john', 'red', 7);
+
+// fs.readFile('./students.txt', (err, data) => {
+//   if (err) throw err;
+//   newBus.studentEntersBus(JSON.parse(data));
+//   // console.log(newBus.studentsOnTheBus);
+// });
+
 addStudent();
 
 
